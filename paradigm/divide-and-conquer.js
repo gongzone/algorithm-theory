3cec510ba26d9147dd51d65dacb10d1c1fefe9dd
dnjsqhwo <dnjsqhwo@gmail.com>
2022-11-18 03:21:48 +0900

    update

---
+++ paradigm/divide-and-conquer.js
@@ -0,0 +1,3 @@
+// 1. Multo-branched recursion.
+// 2. Breaks a problem into multiple smaller but same sub-problems.
+// 3. Combines the solutions of sub-problems into the solution for the original problem.
--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -1,6 +1,4 @@
 // divide-and-conquer 파라다임 적용
-// T: O(nlogn) -> but, worst case일시 O(n^2), worst case: pivot이 가장 크거나 작은 값일 때, 즉 반으로 쪼갤 수 가 없다.
-// S: O(logn)
 
 const array = [5, 3, 1, 6, 4, 2];
 
@@ -26,12 +24,33 @@ const partition = (array, left, right) => {
   return i;
 };
 
+// quick sort
+// T: O(nlogn) -> best case, T: O(n^2) -> worst case, S: O(logn)
+// 정렬 알고리즘
 const quickSort = (array, left, right) => {
   if (left >= right) return;
 
   const partitionIndex = partition(array, left, right);
   quickSort(array, left, partitionIndex - 1);
   quickSort(array, partitionIndex + 1, right);
+};
+
+// quick select
+// T: O(n) -> best case, T: O(n^2) -> worst case, S: O(1)
+// tail recursion이다.
+// 특정 값을 찾는 경우에 쓰는 알고리즘 (예: 배열에서 두번째로 큰 값을 찾으시오.)
+const quickSelect = (array, left, right, idxToFind) => {
+  if (left >= right) return;
+
+  const partitionIndex = partition(array, left, right);
+
+  if (partitionIndex === idxToFind) {
+    return array[partitionIndex];
+  } else if (idxToFind < partitionIndex) {
+    return quickSelect(array, left, partitionIndex - 1, idxToFind);
+  } else {
+    return quickSelect(array, partitionIndex + 1, right, idxToFind);
+  }
 };
 
 quickSort(array, 0, array.length - 1);
